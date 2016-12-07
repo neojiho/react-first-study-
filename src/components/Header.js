934e4ab504ef4ab48f35f46e4385f474b14e9791
hoon <neoleen1124@gmail.com>
2016-12-07 21:51:24 +0900

    add files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class Header extends React.Component {
+    render(){
+        return (
+            <h1>다른 클래스에서 가져온 HEADER</h1>
+        );
+    }
+}
+
+export default Header;
+
